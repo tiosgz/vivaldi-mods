f580cd7deacbb3fdbbd88604cd8d9abafbe8db11
tiosgz <grzybarz@gmail.com>
2020-04-01 19:30:45 +0000

    SD icons update

--- js/sd_icons.js
+++ js/sd_icons.js
@@ -1,21 +1,116 @@
-(function () {
-  function getReactEventHandlers(thumbnail) {
-    for (var key in thumbnail) {
-      if (key.indexOf('__reactEventHandlers') > -1) {
-        return key;
+/*
+ * Speed Dial icons
+ * Written by Tam710562
+ */
+
+window.gnoh = Object.assign(window.gnoh || {}, {
+  createElement: function (tagName, attribute, parent, inner, options) {
+    if (typeof tagName === 'undefined') {
+      return;
+    }
+    if (typeof options === 'undefined') {
+      options = {};
+    }
+    if (typeof options.isPrepend === 'undefined') {
+      options.isPrepend = false;
+    }
+    const el = document.createElement(tagName);
+    if (!!attribute && typeof attribute === 'object') {
+      for (let key in attribute) {
+        if (key === 'text') {
+          el.textContent = attribute[key];
+        } else if (key === 'html') {
+          el.innerHTML = attribute[key];
+        } else if (key === 'style' && typeof attribute[key] === 'object') {
+          for (let css in attribute.style) {
+            el.style[css] = attribute.style[css];
+          }
+        } else if (key === 'events' && typeof attribute[key] === 'object') {
+          for (let event in attribute.events) {
+            if (typeof attribute.events[event] === 'function') {
+              el.addEventListener(event, attribute.events[event]);
+            }
+          }
+        } else if (typeof el[key] !== 'undefined') {
+          el[key] = attribute[key];
+        } else {
+          if (typeof attribute[key] === 'object') {
+            attribute[key] = JSON.stringify(attribute[key]);
+          }
+          el.setAttribute(key, attribute[key]);
+        }
       }
     }
+    if (!!inner) {
+      if (!Array.isArray(inner)) {
+        inner = [inner];
+      }
+      for (let i = 0; i < inner.length; i++) {
+        if (inner[i].nodeName) {
+          el.append(inner[i]);
+        } else {
+          el.append(this.createElementFromHTML(inner[i]));
+        }
+      }
+    }
+    if (typeof parent === 'string') {
+      parent = document.querySelector(parent);
+    }
+    if (!!parent) {
+      if (options.isPrepend) {
+        parent.prepend(el);
+      } else {
+        parent.append(el);
+      }
+    }
+    return el;
+  },
+  getReactEventHandlersKey: function (element) {
+    if (!this.reactEventHandlersKey) {
+      if (!element) {
+        element = document.getElementById('browser');
+      } else if (typeof element === 'string') {
+        element = document.querySelector(element);
+      }
+      if (!element || element.ownerDocument !== document) {
+        return;
+      }
+      this.reactEventHandlersKey = Object.keys(element).find(function (key) {
+        return key.startsWith('__reactEventHandlers');
+      });
+    }
+    return this.reactEventHandlersKey;
+  },
+  observeDOM: function (obj, callback, config) {
+    const obs = new MutationObserver(function (mutations, observer) {
+      if (config) {
+        callback(mutations, observer);
+      } else {
+        if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+          callback(mutations, observer);
+        }
+      }
+    });
+    obs.observe(obj, config || {
+      childList: true,
+      subtree: true
+    });
   }
+});
 
+(function () {
   function addIcon(dial, url) {
     var buttonTitle = dial.querySelector('.button-title');
     if (buttonTitle) {
-      var imgIcon = document.createElement('img');
-      imgIcon.width = 16;
-      imgIcon.height = 16;
-      imgIcon.style.marginRight = '6px';
-      imgIcon.style.verticalAlign = 'top';
-      imgIcon.src = 'chrome://favicon/' + url;
+      var imgIcon = gnoh.createElement('img', {
+        width: 16,
+        height: 16,
+        style: {
+          marginRight: '6px',
+          verticalAlign: 'top'
+        },
+        src: 'chrome://favicon/' + url
+      });
       buttonTitle.insertBefore(imgIcon, buttonTitle.firstChild);
     }
   }
@@ -25,33 +120,16 @@
     if (dials.length > 0) {
       for (var dial of dials) {
         dial.dataset.iconLoaded = true;
-        var thumbnail = dial.querySelector('.thumbnail');
-        if (thumbnail) {
-          var fnKey = getReactEventHandlers(thumbnail);
-          if (fnKey) {
-            var url = thumbnail[fnKey].children[0].props.title;
-            addIcon(dial, url);
-          }
+        var fnKey = gnoh.getReactEventHandlersKey(dial);
+        if (fnKey) {
+          var url = dial[fnKey].children[3].props.bookmark.url;
+          addIcon(dial, url);
         }
       }
     }
   }
 
-  var observeDOM = (function () {
-    return function (obj, callback) {
-      var obs = new MutationObserver(function (mutations, observer) {
-        if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
-          callback();
-        }
-      });
-      obs.observe(obj, {
-        childList: true,
-        subtree: true
-      });
-    };
-  })();
-
-  observeDOM(document, function () {
+  gnoh.observeDOM(document, function () {
     speedDialIcons();
   });
 })();
