6d6cb962c741a265c4ab66e316c5934f8c870d6b
Bohdan Potměkleč <grzybarz@gmail.com>
2020-04-23 14:40:53 +0000

    Use events for monitoring window move/resize

--- js/window_size_to_css.js
+++ js/window_size_to_css.js
@@ -7,8 +7,15 @@
             body.style.setProperty('--windowTop',    window.screenTop);
             body.style.setProperty('--windowLeft',   window.screenLeft);
         }
-        setTimeout(update, 1000);
+        // If anything doesn't go well, i.e. it doesn't update,
+        // uncomment the line below
+        // setTimeout(update, 1000);
     }
 
     update();
+    window.addEventListener('resize', update);
+    // TODO: check if it's current window
+    // - the resized window ID is passed to the function
+    // - you can get the ID of current window with v.wP.getCurrentId(callback)
+    vivaldi.windowPrivate.onPositionChanged.addListener(update);
 })();
