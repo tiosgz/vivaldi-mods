b7f6214b5c5dd8f28ac39ca4b83736c138ee42c9
Bohdan Potměkleč <grzybarz@gmail.com>
2020-05-02 13:14:41 +0000

    Fix `window_size_to_css` when the window is minimized etc.

--- js/window_size_to_css.js
+++ js/window_size_to_css.js
@@ -1,15 +1,30 @@
 (function() {
+    let lastX = -1;
+    let lastY = -1;
+    let lastWidth = 0;
+    let lastHeight = 0;
+
     function update() {
         let body = document.body;
         if (body) {
-            body.style.setProperty('--screenHeight', screen.height);
-            body.style.setProperty('--screenWidth',  screen.width);
-            body.style.setProperty('--windowTop',    window.screenTop);
-            body.style.setProperty('--windowLeft',   window.screenLeft);
+            if (screen.width !== lastWidth) {
+                body.style.setProperty('--screenWidth',  screen.width);
+                lastWidth = screen.width;
+            }
+            if (screen.height !== lastHeight) {
+                body.style.setProperty('--screenHeight', screen.height);
+                lastHeight = screen.height;
+            }
+            if (window.screenLeft !== lastX) {
+                body.style.setProperty('--windowLeft',   window.screenLeft);
+                lastX = window.screenLeft;
+            }
+            if (window.screenTop !== lastY) {
+                body.style.setProperty('--windowTop',    window.screenTop); 
+                lastY = window.screenTop;
+            }
         }
-        // If anything doesn't go well, i.e. it doesn't update,
-        // uncomment the line below
-        // setTimeout(update, 1000);
+        setTimeout(update, 250);
     }
 
     update();
